9704c2eb2bded5957e5a2b5ffc31a5de8c846422
Marko Ilioski <markoilioski21@gmail.com>
2023-03-29 00:16:08 +0200

    PRASAJ PROFESOR SO SE SLUCUVA

--- Lab3_2.py
+++ Lab3_2.py
@@ -1,5 +1,9 @@
 from constraint import *
 
+# def check_count(list):
+# for i in list:
+
+
 if __name__ == '__main__':
     num = int(input())
 
@@ -12,7 +16,8 @@ if __name__ == '__main__':
         paper_info = input()
 
     # Tuka definirajte gi promenlivite
-    ...
+    # Poradi nekoja pricina raboti so strings samo do 9
+    # variables = tuple(["1","2", "3","4","5","6","7","8","9","10","11","12","13"])
 
     domain = [f'T{i + 1}' for i in range(num)]
 
@@ -22,9 +27,9 @@ if __name__ == '__main__':
     problem.addVariables(variables, domain)
 
     # Tuka dodadete gi ogranichuvanjata
-    ...
+    # problem.addConstraint()
 
     result = problem.getSolution()
 
     # Tuka dodadete go kodot za pechatenje
-    ...
+    print(result)
