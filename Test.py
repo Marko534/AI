196150f36f36094f27b1969d2abfa9b98a2a3359
Marko Ilioski <markoilioski21@gmail.com>
2023-04-11 23:46:46 +0200

    AAAAAAAAAAAAAAAA

--- Test.py
+++ Test.py
@@ -7,3 +7,12 @@ def add(stat):
 if __name__ == '__main__':
     test = [0] * 10
 
+    n = int(input())
+    l = int(input())
+
+    disc = [0] * l
+    for i in range(n):
+        disc[i] = i + 1
+
+    print(disc)
+    print(disc.index(3))
