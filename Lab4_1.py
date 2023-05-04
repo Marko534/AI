f47e90799f1e1039ee6f6451f4daa973e3f4440a
Marko Ilioski <markoilioski21@gmail.com>
2023-05-04 19:35:21 +0200

    Copied the code from the aud exe

--- Lab4_1.py
+++ Lab4_1.py
@@ -1386,41 +1386,38 @@ if __name__ == '__main__':
                ['H', 'S', 'X', '2', '2', '1', '1', '2', '1', '1', '0'],
                ['C', 'S', 'O', '1', '2', '1', '2', '2', '1', '1', '0']]
 
-encoder = OrdinalEncoder()
-encoder.fit([row[:-1] for row in dataset])
+    encoder = OrdinalEncoder()
+    encoder.fit([row[:-1] for row in dataset])
 
-train_set = dataset[:int(0.7 * len(dataset))]
-train_x = [row[:-1] for row in train_set]
-train_y = [row[-1] for row in train_set]
-train_x = encoder.transform(train_x)
+    train_set = dataset[:int(0.75 * len(dataset))]
+    train_x = [row[:-1] for row in train_set]
+    train_y = [row[-1] for row in train_set]
+    train_x = encoder.transform(train_x)
 
-test_set = dataset[int(0.7 * len(dataset)):]
-test_x = [row[:-1] for row in test_set]
-test_y = [row[-1] for row in test_set]
-test_x = encoder.transform(test_x)
+    test_set = dataset[int(0.75 * len(dataset)):]
+    test_x = [row[:-1] for row in test_set]
+    test_y = [row[-1] for row in test_set]
+    test_x = encoder.transform(test_x)
 
-classifier = CategoricalNB()
-classifier.fit(train_x, train_y)
+    classifier = CategoricalNB()
+    classifier.fit(train_x, train_y)
 
-predicted_class = classifier.predict([test_x[5]])[0]
-true_class = test_y[5]
+    accuracy = 0
 
-accuracy = 0
+    for i in range(len(test_set)):
+        predicted_class = classifier.predict([test_x[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy += 1
 
-for i in range(len(test_set)):
-    predicted_class = classifier.predict([test_x[i]])[0]
-    true_class = test_y[i]
-    if predicted_class == true_class:
-        accuracy += 1
+    accuracy = accuracy / len(test_set)
 
-accuracy = accuracy / len(test_set)
+    print(f'Accuracy: {accuracy}')
 
-print(f'Accuracy: {accuracy}')
+    entry = [el for el in input().split(',')]
 
-entry = [el for el in input().split(',')]
+    encoded_entry = encoder.transform([entry])
 
-encoded_entry = encoder.transform([entry])
+    predicted_class = classifier.predict(encoded_entry)[0]
 
-predicted_class = classifier.predict(encoded_entry)[0]
-
-print(predicted_class)
+    print(predicted_class)
